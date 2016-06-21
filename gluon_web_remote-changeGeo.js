d627f86061347785f689651a7c9a18f77e4114b2
MTRNord <mtrnord1@gmail.com>
2016-06-21 22:38:51 +0200

    Regex fixed

--- gluon_web_remote-changeGeo.js
+++ gluon_web_remote-changeGeo.js
@@ -1,11 +1,12 @@
 var program = require('commander');
 var backend = require("./backend/commands.js");
 var chalk_lib = require('chalk');
+var ipv6 = require('ip-address').Address6;
 var chalk = new chalk_lib.constructor({enabled: true});
 var hostname_val = /^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$/
-var ipv6_val = /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
-var key_val = /^(ssh-[a-z]{3}) ([0-9]{3,4}) ([0-9a-f]{2}:){15}[0-9a-f]{2}$/
-var lat_long_val = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
+var ipv6_val = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+var key_val = /^ssh-[a-zA-Z]{3} AAAA[0-9A-Za-z+/]+[=]{0,3} ([^@]+@[^@]+)$/gm
+var lat_long_val = /^(\-?\d+(\.\d+)?).\s*(\-?\d+(\.\d+)?)$/
 
 
 program
@@ -25,8 +26,9 @@ if (args.length === 5) {
   var lat = args[3]
   var long = args[4]
   var alt = args[5]
-  if (ipv6_val.test(ip) || hostname_val.test(ip)) {
-    if (key_val.test(key)) {
+  var ipv6_val = new ipv6(ip);
+  if (ipv6_val.isValid()) {
+    if (key_val.test(key) || hostname_val.test(ip)) {
       if (lat_long_val.test(lat)) {
         if (lat_long_val.test(long)) {
           backend.changeGeo(ip, key, lat, long, alt, "")
@@ -49,35 +51,37 @@ if (args.length === 5) {
   }
 }
 if (args.length === 7) {
-  var ip = args[1]
-  var key = args[2]
-  var token = args[3]
-  var ccode = args[4]
-  var lat = args[5]
-  var long = args[6]
-  var alt = args[7]
-  //if (ipv6_val.test(ip) || hostname_val.test(ip)) {
+  var ip = args[0]
+  var key = args[1]
+  var token = args[2]
+  var ccode = args[3]
+  var lat = args[4]
+  var long = args[5]
+  var alt = args[6]
+  var ipv6_val = new ipv6(ip);
+  if (ipv6_val.isValid() || hostname_val.test(ip)) {
     if (key_val.test(key)) {
       if (lat_long_val.test(lat)) {
         if (lat_long_val.test(long)) {
           backend.changeGeo(ip, key, lat, long, alt, token, ccode)
           console.log(chalk.green("Geolocation changed successfully!"));
         }else {
-          console.error(chalk.bold.red('Please set correct Latitude (See http://stackoverflow.com/a/18690202/4929236)'));
+          console.error(chalk.bold.red('Please set correct Longitude (See http://stackoverflow.com/a/18690202/4929236)'));
           process.exit(1);
         }
       }else {
-        console.error(chalk.bold.red('Please set correct Email'));
+        console.error(chalk.bold.red('Please set correct Latitude (See http://stackoverflow.com/a/18690202/4929236)'));
         process.exit(1);
       }
     }else {
-      console.error(chalk.bold.red('Please set correct SSH Key'));
-      process.exit(1);
+      console.error(chalk.bold.red(key));
+      // console.error(chalk.bold.red('Please set correct SSH Key'));
+      // process.exit(1);
     }
-  // }else {
-  //   console.error(chalk.bold.red('Please set correct IPv6 or Hostname'));
-  //   process.exit(1);
-  // }
+  }else {
+    console.error(chalk.bold.red('Please set correct IPv6 or Hostname'));
+    process.exit(1);
+  }
 }
 
 console.log();
